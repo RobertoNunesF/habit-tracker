736b98ef86fa94147aa3fcaec7eac2c430d841d4
RobertoFerreira <rrobertonunesferreira@gmail.com>
2026-06-11 23:29:13 -0300

    feat: setup tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,12 @@
-<h1>
-    Welcome to the Habit Tracker App!
-</h1>
-
-<p>
-    Olá, {{ $name }}!
-</p>
-<p>
-    Seus hábitos são:
-</p>
-<ul>
-    @foreach ($habits as $habit)
-        <li>{{ $habit }}</li>
-    @endforeach
-</ul>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>{{config('app.name')}}</title>
+</head>
+<body>
+    <h1 class="text-2xl font-bold underline">Hello World</h1>
+</body>
+</html>
