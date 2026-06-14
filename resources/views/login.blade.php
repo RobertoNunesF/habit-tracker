9f4b4f483d6bd9bb5f507ef50e0781003d38cf61
RobertoFerreira <rrobertonunesferreira@gmail.com>
2026-06-13 22:21:09 -0300

    feat(LoginRequest): create form request and improve show error

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,29 +1,51 @@
 <x-layout>
     <main class="py-10">
-       <h1>Faça login</h1>
+        
+        <section class="mt-4 bg-white max-w-150 mx-auto border-2 p-10">
+           <h1 class="font-bold text-4xl mb-4">Faça login</h1>
 
-       <section class="mt-4">
-        <form action="/login" method="POST">
+            <p>Insira seus dados para acessar</p>
+
+
+        <form action="/login" method="POST" class="mt-4 flex flex-col gap-4">
             @csrf
 
 
-            @error('email')
-                <p class="text-red-500 text-xl mt-1">{{ $message }}</p>
-            @enderror
+            <div class="flex flex-col gap-4 mt-4">
+            <label for="email">Email</label>
 
             <input 
             type="email" 
             name="email" 
             placeholder="your@email.com" 
-            class="bg-white p-2 border-2"
+            class="bg-white p-2 border-2 @error ('email') border-red-500 @enderror"
             >
+
+
+            @error('email')
+            <p class="text-red-500 text-sm">
+                {{ $message }}
+            </p>
+            @enderror
+            </div>
+
+            <div class="flex flex-col gap-4 mt-4">
+            <label for="password">Senha</label>
 
             <input 
             type="password" 
             name="password" 
             placeholder="********" 
-            class="bg-white p-2 border-2"
+            class="bg-white p-2 border-2 @error ('password') border-red-500 @enderror"
             >
+
+
+            @error('password')
+            <p class="text-red-500 text-sm">
+                {{ $message }}
+            </p>
+            @enderror
+            </div>
 
             <button 
             type="submit"
